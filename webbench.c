59fabc065e10788a09c9ae85551c030626293046
Runzhen Wang <runzhew@g.clemson.edu>
2014-10-24 02:57:54 -0400

    bench() finished

--- webbench.c
+++ webbench.c
@@ -298,4 +298,97 @@ void build_request(const char *url)
 	// printf("Req=%s\n",request); 
 }
 
+/* vraci system rc error kod */
+static int bench(void)
+{
+	int i, j, k;
+	pid_t pid = 0;
+	FILE *f;
+
+	/* check avaibility of target server */
+	i = Socket((proxyhost == NULL) ? host : proxyhost, proxyport);
+
+	if (i < 0) {
+		fprintf(stderr,
+			"\nConnect to server failed. Aborting benchmark.\n");
+		return 1;
+	}
+	close(i);
+	
+	/* create pipe */
+	if (pipe(mypipe)) {
+		perror("pipe failed.");
+		return 3;
+	}
+	
+	/* fork childs */
+	for (i = 0; i < clients; i++) {
+		pid = fork();
+		if (pid <= (pid_t)0) {
+			/* child process or error*/
+			sleep(1); /* make childs faster */
+			break;
+		}
+	}
+
+	if (pid < (pid_t) 0) {
+		fprintf(stderr,"problems forking worker no. %d\n",i);
+		perror("fork failed.");
+		return 3;
+	}
+
+	if (pid == (pid_t)0) {
+		/* I am a child */
+		if (proxyhost == NULL) {
+			benchcore(host, proxyport, request);
+		}
+		else {
+			benchcore(proxyhost, proxyport, request);
+		}
+		/* write results to pipe */
+		f = fdopen(mypipe[1], "w");
+		if (f == NULL) {
+			perror("open pipe for writing failed.");
+			return 3;
+		}
+		/* fprintf(stderr,"Child - %d %d\n",speed,failed); */
+		fprintf(f, "%d %d %d\n", speed, failed, bytes);
+		fclose(f);
+		return 0;
+	}	
+	else {
+		f = fdopen(mypipe[0],"r");
+		if (f == NULL) {
+			perror("open pipe for reading failed.");
+			return 3;
+		}
+		setvbuf(f,NULL,_IONBF,0);
+		speed = 0; 
+		failed = 0;
+		bytes = 0;
+
+		while(1) {
+			pid=fscanf(f,"%d %d %d",&i,&j,&k);
+			if (pid < 2) {
+				fprintf(stderr,"Some of our childrens died.\n");
+				break;
+			}
+			speed += i;
+			failed += j;
+			bytes += k;
+			/* fprintf(stderr,
+			 * "*Knock* %d %d read=%d\n",speed,failed,pid); */
+
+			if (--clients == 0)
+				break;
+		}
+		fclose(f);
+		printf("\nSpeed=%d pages/min, %d bytes/sec.\n
+		       Requests: %d susceed, %d failed.\n",
+		      (int)((speed+failed)/(benchtime/60.0f)),
+		     (int)(bytes/(float)benchtime), speed, failed);
+	}
+	return i;
+}
+
 
